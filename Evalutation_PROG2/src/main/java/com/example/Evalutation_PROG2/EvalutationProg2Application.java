5913a251a53d70315403073d2a3fc49db1dd4378
antso.tech <hei.antso.2007@gmail.com>
2026-03-11 16:25:56 +0300

    ajout du dossier Evaluation PROG2

---
+++ Evalutation_PROG2/src/main/java/com/example/Evalutation_PROG2/EvalutationProg2Application.java
@@ -0,0 +1,13 @@
+package com.example.Evalutation_PROG2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalutationProg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalutationProg2Application.class, args);
+	}
+
+}
---
+++ Evalutation_PROG2/src/test/java/com/example/Evalutation_PROG2/EvalutationProg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Evalutation_PROG2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalutationProg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
