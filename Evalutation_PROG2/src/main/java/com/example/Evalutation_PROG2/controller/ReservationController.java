bfd0d863ea36307d8165973fd0ec874905807d8f
antso.tech <hei.antso.2007@gmail.com>
2026-03-12 13:37:29 +0300

    ajout des conditions pour la disponiblité des salles

--- Evalutation_PROG2/src/main/java/com/example/Evalutation_PROG2/controller/ReservationController.java
+++ Evalutation_PROG2/src/main/java/com/example/Evalutation_PROG2/controller/ReservationController.java
@@ -1,7 +1,11 @@
 package com.example.Evalutation_PROG2.controller;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +13,7 @@ import com.example.Evalutation_PROG2.entities.*;
 import com.example.Evalutation_PROG2.services.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @RestController
@@ -21,14 +26,20 @@ public class ReservationController {
         return repositoryService.getAllReservation();
     }
 
+
+    @SuppressWarnings("unlikely-arg-type")
     @PostMapping("/booking")
-    public List<Reservation> createNewReservation(@RequestBody List<Reservation> reservation) {
-        boolean availability = true;
-        if (availability) {
+    public ResponseEntity<?> createNewReservation(@RequestBody List<Reservation> reservation) {
+        boolean is_available = repositoryService.getAllReservation().stream().allMatch(previousReservation -> previousReservation.getDate().equals(reservation.stream().map(nextReservation -> nextReservation.getDate())));
+        if (!is_available) {
+            return new ResponseEntity<>("Cette salle est déja disponible", HttpStatusCode.valueOf(409));
+            
+        }else{
+            List<Reservation> addReservation = this.repositoryService.addReservation(reservation);
+            return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(addReservation);
             
         }
-        List<Reservation> addReservation = this.repositoryService.addReservation(reservation);
-        return addReservation;
+        
         
     }
 
