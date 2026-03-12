34d38bdd1a214920a7e63204db2b25a9fd01e452
antso.tech <hei.antso.2007@gmail.com>
2026-03-12 10:05:22 +0300

    finition des getReservation sans test ni memoire vive

--- Evalutation_PROG2/src/main/java/com/example/Evalutation_PROG2/controller/ReservationController.java
+++ Evalutation_PROG2/src/main/java/com/example/Evalutation_PROG2/controller/ReservationController.java
@@ -1,12 +1,25 @@
 package com.example.Evalutation_PROG2.controller;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Evalutation_PROG2.entities.*;
-import com.example.Evalutation_PROG2.Repository.*;;
+import com.example.Evalutation_PROG2.services.*;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@RestController
 public class ReservationController {
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @GetMapping("/findAll")
+    public List<Reservation> getMethodName(@RequestParam String param) {
+        return repositoryService.getAllReservation();
+    }
+    
 
 }
